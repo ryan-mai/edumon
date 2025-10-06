b71b998fe345b5ccfef88b30e74d8dc2d6ea602b
Your NameRyan <inoober2008@gmail.com>
2025-10-05 20:04:42 -0400

    Added background image to make the game feel more roomy

--- main.js
+++ main.js
@@ -9,6 +9,21 @@ setBackground(Color.fromHex('#36A6E0'))
 
 loadAssets();
 
+document.addEventListener('DOMContentLoaded', () => {
+    Object.assign(document.body.style, {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        margin: '0',
+        backgroundImage: "url('./assets/sky-background.png')",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+    });
+});
+
+
 scene('world', (worldState) => setWorld(worldState));
 scene('battle', (worldState) => setBattle(worldState));
 scene('blackjack', (worldState) => setBlackjack(worldState));
